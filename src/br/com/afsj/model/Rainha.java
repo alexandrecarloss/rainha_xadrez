7afe2ecd66e4d638190099ea0c2820a9f0ca39f1
alexandrecarloss <107065086+alexandrecarloss@users.noreply.github.com>
2023-12-12 00:56:19 -0400

    Rainha

---
+++ src/br/com/afsj/model/Rainha.java
@@ -0,0 +1,81 @@
+package br.com.afsj.model;
+
+public class Rainha extends Peca {
+	@Override
+	public boolean movimentoOK(int x, int y) {
+		
+	if (posX == -1 && posY == -1) {
+            return true;
+        }
+        
+        int variaX = Math.abs(x - posX);
+        int variaY = Math.abs(y - posY);
+        int pX, pY;
+        if(x == posX) {
+            if(y > posY) {
+                pY = 1;
+            } else {
+                pY = -1;
+            }            
+            int j = posY + pY;
+            //Verificando se existe alguma peça no caminho
+            while (j != y) {
+                Peca pBranca = Tabuleiro.listaBrancas.pecaPosicao(x, j);
+                Peca pPreta = Tabuleiro.listaPretas.pecaPosicao(x, j);
+                if (pBranca != null || pPreta != null) {
+                    return false; 
+                }
+                j += pY;
+            }
+        }
+        
+        if(y == posY) {
+            if(x > posX) {
+                pX = 1;            
+            } else {
+                pX = -1;
+            }
+            int i = posX + pX;
+            //Verificando se existe alguma peça no caminho
+            while (i != x) {
+                Peca pBranca = Tabuleiro.listaBrancas.pecaPosicao(i, y);
+                Peca pPreta = Tabuleiro.listaPretas.pecaPosicao(i, y);
+                if (pBranca != null || pPreta != null) {
+                    return false; 
+                }
+                i += pX;
+            }
+        }
+        
+        if (variaX == variaY) {           
+            
+            if (x > posX) {
+                pX = 1;
+            } else {
+                pX = -1;
+            }
+            if (y > posY) {
+                pY = 1;
+            } else {
+                pY = -1;
+            }
+            
+            int i = posX + pX;
+            int j = posY + pY;
+            
+            //Verificando se existe alguma peça no caminho
+            while (i != x && j != y) {
+                Peca pBranca = Tabuleiro.listaBrancas.pecaPosicao(i, j);
+                Peca pPreta = Tabuleiro.listaPretas.pecaPosicao(i, j);
+                if (pBranca != null || pPreta != null) {
+                    return false; 
+                }
+                i += pX;
+                j += pY;
+            }
+            return true;
+        }
+        return false;
+    }
+
+}
